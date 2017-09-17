250d677dbcbe26e0b7b4c413c522a78c0b35cc4f
triebjlr@gmail.com <triebjlr@gmail.com>
2017-09-16 22:50:46 -0500

    simple cfg changes

--- otsu-thresholding.py
+++ otsu-thresholding.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./images/fap.jpg',0)
+img = cv2.imread('./images/perspective-01-small.jpg',0)
 
 # global thresholding
 ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
 # Otsu's thresholding
-ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ret2,th2 = cv2.threshold(img,128,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 # Otsu's thresholding after Gaussian filtering
 blur = cv2.GaussianBlur(img,(5,5),0)
--- simple-thresholding.py
+++ simple-thresholding.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./images/fap.jpg',0)
-ret,thresh1 = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,50,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,50,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,50,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,50,255,cv2.THRESH_TOZERO_INV)
+img = cv2.imread('./images/perspective-01-small.jpg',0)
+ret,thresh1 = cv2.threshold(img,170,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,170,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(img,170,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(img,170,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(img,170,255,cv2.THRESH_TOZERO_INV)
 
 titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
