23c2e80e45584b51b5a08f924fb66cd4abf02b46
jorgelopezrivas <triebjlr@gmail.com>
2017-10-12 15:40:05 -0500

    improvement on this two examples

--- capture-frames.py
+++ capture-frames.py
@@ -1,24 +1,37 @@
 import numpy as np
 import cv2
+import time
 from matplotlib import pyplot as plt
 
-# Capture certain number of frames
-frames = 9
-path = './saved_frames/'
+# Capture x seconds of video as frames
+captureTime =  0.067
+capture = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
-width = cap.get(3)  
-height = cap.get(4)
+# Find OpenCV version and get fps
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+if int(major_ver)  < 3 :
+	fps = capture.get(cv2.cv.CV_CAP_PROP_FPS)
+	print "Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps)
+else :
+	fps = capture.get(cv2.CAP_PROP_FPS)
+	print "Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps)
+     
+
+frames = captureTime * fps
+dontSaveFrames = 10
+path = './saved_frames/'
+width = capture.get(3)  
+height = capture.get(4)
 average = np.zeros((height, width), np.uint8)
 i = 0
 
-while(i < frames):
-	ret,frame = cap.read()
-	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	cv2.imwrite(path + "frame-%d.jpg" % i, gray)
-	average = average + (gray / frames)
+while(i < frames + dontSaveFrames):
+	ret,frame = capture.read()
+	if(i >= dontSaveFrames):	
+		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		cv2.imwrite(path + "frame-%d.jpg" % (i-dontSaveFrames), gray)
+		average = average + (gray / frames)
 	i += 1
 
-cap.release()
-cv2.imwrite(path + "combined.jpg", average)
-
+capture.release()
+cv2.imwrite(path + "average.jpeg", average)
--- noise.py
+++ noise.py
@@ -6,34 +6,26 @@ from matplotlib import pyplot as plt
 # Types of noise: Gaussian, Rayleigh, Gamma, Exponential, Uniform, Impulse (salt & pepper)
 
 
-img = cv2.imread('./images/standard_images/cameraman.tif',0)
-#generate different types of noise:
+img = cv2.imread('./images/standard_images/fig0503.tif',0)
 
 # Gaussian:
 # randn fills the array with normally distributed random numbers
 # randn(dest, mean, stddev)
 gaussian_noise = np.zeros((img.shape[0], img.shape[1]), np.uint8)
-cv2.randn(gaussian_noise, (1), (30))
+cv2.randn(gaussian_noise, (128), (10))
 res1 = img + gaussian_noise
-
+median = cv2.medianBlur(res1,29)
 
 # plot all the images and their histograms
-images = [img, gaussian_noise, res1]
+images = [img, gaussian_noise, res1, median]
 titles = ['Original Image','Histogram','Global Thresholding (v=127)']
 #          'Original Noisy Image','Histogram',"Otsu's Thresholding",
 #          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
 
-
-
-#plt.subplot(1,2,1)
-#plt.imshow(img2, 'gray')
-#plt.subplot(1,2,2)
-#plt.hist(img2.ravel(), 256)		
-#plt.show()
-
-for i in xrange(3):
+nrows = 4
+for i in xrange(nrows):
     #plt.subplot(nrows, ncols, plot_number)
-    plt.subplot(3,2,i*2+1),plt.imshow(images[i],'gray')
+    plt.subplot(nrows,2,i*2+1),plt.imshow(images[i],'gray')
     #ravel returns a contiguous flattened array
-    plt.subplot(3,2,i*2+2),plt.hist(images[i].ravel(),256)
+    plt.subplot(nrows,2,i*2+2),plt.hist(images[i].ravel(),256, [0,256])
 plt.show()
