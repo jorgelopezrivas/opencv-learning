602a64735c36103afff3e502207a0bfa2e76f805
jorgelopezrivas <triebjlr@gmail.com>
2017-10-13 12:13:56 -0500

    Code refactor for better organization. Added uniform_noise method

--- noise.py
+++ noise.py
@@ -1,31 +1,69 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
 # Noise Exercises
-# Types of noise: Gaussian, Rayleigh, Gamma, Exponential, Uniform, Impulse (salt & pepper)
+# Types of noise: 
+#	1) Gaussian
+#	2) Rayleigh, 
+#	3) Gamma, 
+#	4) Exponential, 
+#	5) Uniform, 
+#	6) Impulse (salt & pepper)
 
+def gaussian_noise(src, mean, stddev):
+	noise = np.zeros((src.shape[0], src.shape[1]), np.uint8)
+	# randn fills the array with normally distributed random numbers
+	# randn(dest, mean, stddev)
+	cv2.randn(noise, (mean), (stddev))	
+	dst = src + noise
+	return dst, noise
+	
+def rayleigh_noise()
 
+	return dst, noise
+	
+def gamma_noise()
+
+	return dst, noise
+	
+def exponential_noise()
+
+	return dst, noise
+	
+def uniform_noise(src, low, high):
+	noise = np.random.uniform(low, high, src.shape[0]*src.shape[1])
+	noise = noise.reshape(src.shape[0], src.shape[1]).astype(np.uint8)
+	dst = src + noise	
+	return dst, noise
+	
+def impulse_noise()
+
+	return dst, noise
+	
 img = cv2.imread('./images/standard_images/fig0503.tif',0)
-
-# Gaussian:
-# randn fills the array with normally distributed random numbers
-# randn(dest, mean, stddev)
-gaussian_noise = np.zeros((img.shape[0], img.shape[1]), np.uint8)
-cv2.randn(gaussian_noise, (128), (10))
-res1 = img + gaussian_noise
-median = cv2.medianBlur(res1,29)
-
-# plot all the images and their histograms
-images = [img, gaussian_noise, res1, median]
-titles = ['Original Image','Histogram','Global Thresholding (v=127)']
-#          'Original Noisy Image','Histogram',"Otsu's Thresholding",
-#          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
-
-nrows = 4
-for i in xrange(nrows):
-    #plt.subplot(nrows, ncols, plot_number)
-    plt.subplot(nrows,2,i*2+1),plt.imshow(images[i],'gray')
-    #ravel returns a contiguous flattened array
-    plt.subplot(nrows,2,i*2+2),plt.hist(images[i].ravel(),256, [0,256])
-plt.show()
+plot = 1
+plots = ['Gaussian Noise', 'Uniform Noise']
+titles = ['Original', 'Noise', 'Noise Added', 'Media Filter']
+for _ in plots:
+	nrows = 4	
+	plt.figure(plot)
+	if plot == 1:
+		# Gaussian
+		res, noise = gaussian_noise(img, 0, 20)
+	elif plot == 2:
+		# Uniform
+		res, noise = uniform_noise(img, 50, 100)
+		
+	median = cv2.medianBlur(res, 11)
+	images = [img, noise, res, median]
+	
+	for i in xrange(nrows):
+		#plt.subplot(nrows, ncols, plot_number)
+		plt.subplot(nrows, 2, i*2+1),plt.imshow(images[i], 'gray')
+		plt.xticks([]),plt.yticks([])
+		plt.title(titles[i])
+		#ravel returns a contiguous flattened array
+		plt.subplot(nrows,2,i*2+2),plt.hist(images[i].ravel(),256, [0,256])
+		plt.xlim(0,255)
+	plt.show()
+	plot += 1
