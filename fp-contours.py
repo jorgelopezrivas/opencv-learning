62fcee3fd442dc0cd0337c1b3efe094227aa55c9
jorgelopezrivas <triebjlr@gmail.com>
2017-11-28 10:52:20 -0600

    last version of fp-contours.py

--- fp-contours.py
+++ fp-contours.py
@@ -6,7 +6,7 @@ def distance(a, b):
 	dist = sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 	return dist
 	
-def getRectangleDimentions(box, angle):
+def get_rectangle_dimentions(box, angle):
 	#receives two values, return height = largest number and width = smallest
 	side_a = distance(box[0], box[1])
 	side_b = distance(box[1], box[2])
@@ -27,24 +27,28 @@ def getRectangleDimentions(box, angle):
 	
 	return w,h, ret_angle
 	
-def fixRotation(img):
+def fix_rotation(img):
 	fp_contour = get_fingerprint_contour(img)
 	rect = cv2.minAreaRect(fp_contour)
-
 	box = cv2.boxPoints(rect)
-	box = np.int0(box)
-	w,h,angle = getRectangleDimentions(box, rect[2])
-	
-	
-	#cv2.drawContours(img,[box],0,(0,0,255),2)
-	#cv2.imshow('rect', img)
+	w,h,angle = get_rectangle_dimentions(box, rect[2])
 	
 	rotations = []
 	rows,cols = img.shape[0], img.shape[1]
+	
+	# Obtener la imagen rotada 1/2
 	M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
-	rotations.append(cv2.warpAffine(img,M,(cols,rows)))
+	img_rot = cv2.warpAffine(img,M,(cols,rows))
+	img_crop = crop_min_area_rect(img_rot,box,M)
+	rotations.append(img_crop)	
+	
+	# Obtener la imagen rotada 2/2
 	M = cv2.getRotationMatrix2D((cols/2,rows/2),angle+180,1)
-	rotations.append(cv2.warpAffine(img,M,(cols,rows)))
+	img_rot = cv2.warpAffine(img,M,(cols,rows))
+	img_crop = crop_min_area_rect(img_rot,box,M)
+	rotations.append(img_crop)
+	
+
 	return rotations
 			
 def get_fingerprint_contour(img):
@@ -68,17 +72,30 @@ def get_fingerprint_contour(img):
 
 	return largest_area
 	
-def crop_monAreaRect():
+def crop_min_area_rect(img,box,M):
+	
 	# rotate bounding box
-	#rect0 = (rect[0], rect[1], 0.0)
-	#box = cv2.boxPoints(rect)
-	#pts = np.int0(cv2.transform(np.array([box]), M))[0]    
-	#pts[pts < 0] = 0
-
+	pts = np.int0(cv2.transform(np.array([box]), M))[0] 
+	pts[pts < 0] = 0
+	
+	# get min and max values for x and y
+	min_val = np.amin(pts, axis=0)
+	max_val = np.amax(pts, axis=0)
+	
 	# crop
-	#img_crop = img_rot[pts[1][1]:pts[0][1], pts[1][0]:pts[2][0]]
-	img_crop = img_rot
+	img_crop = img[min_val[1]:max_val[1], min_val[0]:max_val[0]]
+		
 	return img_crop
+	
+def copy_image_centered(small, big):
+	# paste small img in the center of big
+	x_pos = (big.shape[1] / 2) - (small.shape[1] / 2)
+	y_pos = (big.shape[0] / 2) - (small.shape[0] / 2)
+	big[y_pos:y_pos + small.shape[0], x_pos:x_pos + small.shape[1]] = small
+	return big
+	
+#def save_image(image):
+
 
 ########################################################################
 # MAIN    
@@ -86,13 +103,20 @@ def crop_monAreaRect():
 
 #https://www.packtpub.com/books/content/fingerprint-detection-using-opencv-3
 
+original = cv2.imread('images/01_m145deg.bmp',1)
+original = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
 
-original = cv2.imread('images/01_117deg.bmp',1)
-img = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
-rotated = fixRotation(img)
+#create new blank image
+work_img = np.zeros((original.shape[0]*2, original.shape[1]*2,3), np.uint8)
+work_img[:] = (255,255,255)
+work_img = cv2.cvtColor(work_img,cv2.COLOR_BGR2GRAY)
+
+work_img = copy_image_centered(original, work_img)
+rotated = fix_rotation(work_img)
 
 
-cv2.imshow('img',original)
+cv2.imshow('original',original)
+#cv2.imshow('big',work_img)
 cv2.imshow('rotated 1', rotated[0])
 cv2.imshow('rotated 2', rotated[1])
 cv2.waitKey(0)
